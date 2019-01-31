bdd12dec385c6f150f79f9f278c3c4665397a194
wjjgjk <wjjgjk@naver.com>
2019-01-31 19:11:52 +0900

    url.py fixed_2

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,6 +19,6 @@ from django.conf.urls import include, url
 
 urlpatterns = [
     #path(r'^admin/$', admin.site.urls),
-    path(r'admin/', admin.site.urls),
+    path(r'admin', admin.site.urls),
     url(r'', include('blog.urls')),
 ]
